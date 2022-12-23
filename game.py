a5cc06dedc56d64bef2474e8433f4d1312561c16
David <davidpetrosyan.002@gmail.com>
2022-12-23 21:03:07 +0400

    firth commit

--- game.py
+++ game.py
@@ -9,15 +9,9 @@ music_player.geometry("450x350")
 directory = askdirectory()
 os.chdir(directory)
 song_list = os.listdir()
-#sfhdsgvfjdsvfhdsfhsd
+
 #comments
-play_list = tkr.Listbox(music_player, font="Helvetica 12 bold", bg='yellow', selectmode=tkr.SINGLE)
-for item in song_list:
-    pos = 0
-    play_list.insert(pos, item)
-    pos += 1
-pygame.init()
-pygame.mixer.init()
+
 
 def play():
     pygame.mixer.music.load(play_list.get(tkr.ACTIVE))
